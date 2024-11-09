1bc88854feb053a0ce30101ee70465f3abb501f8
Gary Tou <gary@garytou.com>
2024-11-09 01:23:28 -0800

    Add Google Drive API

--- Gemfile
+++ Gemfile
@@ -47,6 +47,10 @@ gem "devise"
 gem "omniauth-rails_csrf_protection" # https://github.com/cookpad/omniauth-rails_csrf_protection
 gem "omniauth-google-oauth2"
 
+# Google Drive API
+gem "google-apis-drive_v3"
+gem "signet" # OAuth 2.0 implementation for Google's APIs
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- app/models/concerns/user/has_google_credentials.rb
+++ app/models/concerns/user/has_google_credentials.rb
@@ -2,10 +2,22 @@ class User
   module HasGoogleCredentials
     extend ActiveSupport::Concern
 
+    TOKEN_CREDENTIAL_URI = "https://oauth2.googleapis.com/token".freeze
+
     included do
       validates_presence_of :provider_refresh_token, if: -> { provider_uid.present? }
-
       encrypts :provider_refresh_token
+    end
+
+    def google_credentials
+      # I'm only using Signet to serve as the authentication object for Google::Apis::DriveV3::DriveService
+      # Signet handles refreshing the access token — in fact, I'm not storing the access token at all.
+      @google_credentials ||= Signet::OAuth2::Client.new(
+        token_credential_uri: TOKEN_CREDENTIAL_URI,
+        client_id: Rails.application.credentials.dig(:google, :oauth_client_id),
+        client_secret: Rails.application.credentials.dig(:google, :oauth_client_secret),
+        refresh_token: provider_refresh_token,
+      )
     end
   end
 end
