13ef1467de9354cb17f7ca7fb8da2d81537777ef
Gary Tou <gary@garytou.com>
2024-11-10 02:15:38 -0800

    WIP extract text from PDF images

---
+++ app/models/textualizer.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class Textualizer
+  def initialize(content)
+    @content = content # may either be an ActiveStorage::Attachment or a String
+    @file = nil
+  end
+
+  def text
+    return @content if @content.is_a?(String)
+
+    @content.open do |file|
+      @file = file
+
+      case @content.content_type
+      when "application/pdf"
+        return handle_pdf
+      when /^image\//
+        return handle_image
+      else
+        raise ArgumentError, "Unsupported content type: #{@content.content_type}"
+      end
+    end
+  end
+
+  def handle_pdf
+    convert_pdf_to_text
+    handle_image
+  end
+
+  def handle_image
+    image = RTesseract.new(@file.path)
+    image.to_s
+  end
+
+  def convert_pdf_to_text
+    image = self.class.pdf_to_image(@file)
+    @file.close
+    @file = image
+  end
+
+  def self.pdf_to_image(file)
+    image = Vips::Image.pdfload(file.path, n: -1, dpi: 300) # -1 means all pages
+    result = Tempfile.create([ "image", ".png" ])
+    image.pngsave(result.path)
+    result
+  end
+end
